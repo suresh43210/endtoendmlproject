a229edeb8a4409cda6a73e38fcbbd6037851282d
Suresh Neupane <117652990+suresh43210@users.noreply.github.com>
2023-12-30 21:44:08 +0545

    save changes

--- template.py
+++ template.py
@@ -1,7 +1,10 @@
 import os
 from pathlib import Path
 import logging
-logging.basicConfig(level=logging.info, format='[%(asctime)s]:')
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
 project_name = "mlProject"
 
 
@@ -28,18 +31,27 @@ list_of_files = [
     "research/trials.ipynb",
     "templates/index.html",
     "test.py"
+
+
 ]
+
+
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
-        os.makedirs(filedir, exist_ok= True)
-        logging.info(f"creating directory; {filedir} for the file {filename}")
 
-        if(not os.path.exists(filepath)) or (os.path.getsize(filepath) ==0):
-            with open(filepath, "w") as f:
-                pass
-            logging.info(f"{filename} is already exist")
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
